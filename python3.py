dbb5f4e76e6e5ecf469e2d08d34c33a8ddd9895a
Shreya Agarwal <65066722+shreya817agarwal@users.noreply.github.com>
2022-01-03 19:48:21 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,7 @@
+name=input("Enter your name:")
+def myname(name):
+    if name == "Shreya":
+        print("Welcome "+name)
+    else:
+        print("Sorry "+name+" ,you are not eligible!")
+myname(name)
